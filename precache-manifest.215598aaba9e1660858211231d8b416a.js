b63dc82c0269c70aa7a4018466fe4b5daf9d54e4
Minji Kim <apiichi21@gmail.com>
2020-10-23 15:12:49 +0900

    Updates

---
+++ precache-manifest.215598aaba9e1660858211231d8b416a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1286447a2247995c9f6d132b4c9bc129",
+    "url": "/apiichi/react-study02.git/index.html"
+  },
+  {
+    "revision": "3dafe3be90ec753193a7",
+    "url": "/apiichi/react-study02.git/static/css/main.da4e9442.chunk.css"
+  },
+  {
+    "revision": "f1b97dc8d5ce8967b879",
+    "url": "/apiichi/react-study02.git/static/js/2.b3d3bc0a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/apiichi/react-study02.git/static/js/2.b3d3bc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dafe3be90ec753193a7",
+    "url": "/apiichi/react-study02.git/static/js/main.01676889.chunk.js"
+  },
+  {
+    "revision": "83e4526116cffa602e27",
+    "url": "/apiichi/react-study02.git/static/js/runtime-main.f4d7fb95.js"
+  }
+]);
